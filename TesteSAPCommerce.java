ebaec12d31372cc0591f05641e62ec2c025e3c07
dafix041 <vcruzz1977@gmail.com>
2023-01-16 12:09:26 -0300

    Adicionando ao repositorio

---
+++ TesteSAPCommerce.java
@@ -0,0 +1,11 @@
+public class TesteSAPCommerce{
+    public static void main (String[]args){
+        SAPCommerce sapcommerce = new SAPCommerce();
+
+    sapcommerce.addToCart();
+    sapcommerce.removeItemFromCart();
+
+    }
+
+
+}
